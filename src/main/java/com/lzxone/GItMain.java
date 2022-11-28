451bb69db39411f98f384df37b7aee743d0145ce
lzxone <3139556724@qq.com>
2022-11-28 19:18:19 +0800

    3th

--- src/main/java/com/lzxone/GItMain.java
+++ src/main/java/com/lzxone/GItMain.java
@@ -4,5 +4,6 @@ public class GItMain {
     public static void main(String[] args) {
         System.out.println("1th");
         System.out.println("2th");
+        System.out.println("3th");
     }
 }
